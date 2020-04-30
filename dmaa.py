8c12fd787db3cb27d0c0346d8fb9eb9c63530796
Piotr Fusik <fox@scene.pl>
2020-04-30 12:23:12 +0200

    Add copyright header.

--- dmaa.py
+++ dmaa.py
@@ -1,3 +1,22 @@
+# dmaa.py - Data Matrix ASCII Art encoder
+#
+# Copyright (C) 2020  Piotr Fusik
+#
+# This file is part of DataMatrix.ci, see http://github.com/pfusik/datamatrix-ci
+#
+# DataMatrix.ci is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# DataMatrix.ci is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with DataMatrix.ci.  If not, see http://www.gnu.org/licenses/
+
 import sys
 from datamatrix import DataMatrixEncoder
 
