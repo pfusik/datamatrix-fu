cb914d7a23cdb1360c537a7b99666a9d8306e89b
Piotr Fusik <fox@scene.pl>
2022-11-04 07:39:27 +0100

    Modernize JavaScript.

--- html5datamatrix.js
+++ html5datamatrix.js
@@ -1,21 +1,21 @@
 function redraw()
 {
-	var message = document.getElementById("message").value;
-	var status = document.getElementById("status");
-	var encoder = new DataMatrixEncoder();
+	const message = document.getElementById("message").value;
+	const status = document.getElementById("status");
+	const encoder = new DataMatrixEncoder();
 	try {
 		encoder.encode(message);
-		var columns = encoder.getColumns();
-		var rows = encoder.getRows();
+		const columns = encoder.getColumns();
+		const rows = encoder.getRows();
 
-		var canvas = document.getElementById("canvas");
-		var context = canvas.getContext("2d");
+		const canvas = document.getElementById("canvas");
+		const context = canvas.getContext("2d");
 		context.setTransform(1, 0, 0, 1, 0, 0);
 		context.clearRect(0, 0, canvas.width, canvas.height);
-		var scale = canvas.width / columns;
+		const scale = canvas.width / columns;
 		context.setTransform(scale, 0, 0, scale, 0.5, 0.5);
-		for (var row = 0; row < rows; row++) {
-			for (var column = 0; column < columns; column++) {
+		for (let row = 0; row < rows; row++) {
+			for (let column = 0; column < columns; column++) {
 				context.fillStyle = encoder.getModule(column, row) == 0 ? "#fff" : "#000";
 				context.fillRect(column, row, 1, 1);
 			}
