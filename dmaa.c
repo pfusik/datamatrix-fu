46e40b57ad599de602d3b07e0696b709a6036a6f
Piotr Fusik <piotr@fusion-lang.org>
2023-08-25 10:01:34 +0200

    Update copyright headers.

--- dmaa.c
+++ dmaa.c
@@ -1,22 +1,22 @@
 /*
  * dmaa.c - Data Matrix ASCII Art encoder
  *
- * Copyright (C) 2013-2020  Piotr Fusik
+ * Copyright (C) 2013-2023  Piotr Fusik
  *
- * This file is part of DataMatrix.ci, see http://github.com/pfusik/datamatrix-ci
+ * This file is part of DataMatrix.fu, see http://github.com/pfusik/datamatrix-fu
  *
- * DataMatrix.ci is free software: you can redistribute it and/or modify
+ * DataMatrix.fu is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * DataMatrix.ci is distributed in the hope that it will be useful,
+ * DataMatrix.fu is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with DataMatrix.ci.  If not, see http://www.gnu.org/licenses/
+ * along with DataMatrix.fu.  If not, see http://www.gnu.org/licenses/
  */
 
 #include <stdio.h>
